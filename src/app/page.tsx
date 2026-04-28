4c9750aeaf916425f6cde6e7fbe05e1f5acf7c7b
alainiranzi <alainiranzi00@gmail.com>
2026-04-28 16:27:21 +0200

    Fix formatting in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function LoginPage() {
 
       
       <div className="relative z-10 w-full max-w-6xl h-[90vh] flex rounded-3xl overflow-hidden shadow-2xl bg-white/60 backdrop-blur-xl">
-        {/* LEFT SIDE (UNCHANGED) */}
+      
         <div className="w-1/2 flex flex-col justify-center px-12">
           <div className="flex flex-col items-center mb-8">
             <img src="/logo.svg" className="w-14 h-14 mb-2" />
