e855296f7f0d370ad72ace550a74b8cc01d4911d
alainiranzi <alainiranzi00@gmail.com>
2026-05-24 20:21:30 +0200

    Fix text color in terms and conditions paragraph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function LoginPage() {
             <LoginForm />
           </div>
 
-          <p className="text-center text-sm mt-10 text-gray-599">
+          <p className="text-center text-sm mt-10 text-gray-598">
             Terms & Conditions - Privacy Policy
           </p>
         </div>
